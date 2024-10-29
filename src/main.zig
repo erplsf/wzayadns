7e8fba6ee6d1178983d0de797c78c8f37dbc3e12
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-10-29 21:34:28 +0100

    receive and respond (plus basic types)

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,28 @@ const std = @import("std");
 const net = std.net;
 const posix = std.posix;
 
+// DNS Request/Response
+const RRType = enum(u1) {
+    Query = 0,
+    Response = 1,
+};
+
+const Opcode = enum(u4) {
+    Query = 0,
+    InverseQuery = 1,
+    ServerStatusRequest = 2,
+    _, // all other values (3-15) are reserved for now
+};
+
+const Header = packed struct {
+    id: u16,
+    type: RRType,
+};
+
+const RR = packed struct {
+    header: Header,
+};
+
 pub fn main() !void {
     const address = try std.net.Address.parseIp("127.0.0.1", 5353);
 
@@ -16,8 +38,11 @@ pub fn main() !void {
     var buf: [512]u8 = undefined;
 
     while (true) {
-        const read = posix.read(socket, &buf) catch |err| {
-            std.debug.print("error writing: {}\n", .{err});
+        var client_address: net.Address = undefined;
+        var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+
+        const read = posix.recvfrom(socket, &buf, 0, &client_address.any, &client_address_len) catch |err| {
+            std.debug.print("error reading: {}\n", .{err});
             continue;
         };
 
@@ -25,6 +50,20 @@ pub fn main() !void {
             continue;
         }
 
-        std.debug.print("{any}", .{buf[0..read]});
+        std.debug.print("[{}] -> ", .{client_address});
+
+        const data = buf[0..read];
+
+        std.debug.print("{s}\n", .{std.fmt.fmtSliceHexLower(data)});
+
+        const wrote = posix.sendto(socket, data, 0, &client_address.any, client_address_len) catch |err| {
+            std.debug.print("error writing: {}\n", .{err});
+            continue;
+        };
+
+        if (wrote != read) {
+            std.debug.print("couldn't write the whole response back, exiting!", .{});
+            break;
+        }
     }
 }
