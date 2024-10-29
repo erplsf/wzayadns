490233f24709f274ca27a0517a49f4015ea4c8b3
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-10-29 19:20:41 +0100

    read the data and print it

--- src/main.zig
+++ src/main.zig
@@ -3,10 +3,28 @@ const net = std.net;
 const posix = std.posix;
 
 pub fn main() !void {
-    const address = try std.net.Address.parseIp("127.0.0.1", 53);
+    const address = try std.net.Address.parseIp("127.0.0.1", 5353);
 
     const tpe: u32 = posix.SOCK.DGRAM;
     const protocol = posix.IPPROTO.UDP;
-    const listener = try posix.socket(address.any.family, tpe, protocol);
-    defer posix.close(listener);
+    const socket = try posix.socket(address.any.family, tpe, protocol);
+    defer posix.close(socket);
+
+    try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+    try posix.bind(socket, &address.any, address.getOsSockLen());
+
+    var buf: [512]u8 = undefined;
+
+    while (true) {
+        const read = posix.read(socket, &buf) catch |err| {
+            std.debug.print("error writing: {}\n", .{err});
+            continue;
+        };
+
+        if (read == 0) {
+            continue;
+        }
+
+        std.debug.print("{any}", .{buf[0..read]});
+    }
 }
