1c3c98c69d000f57f378ef798d43d3d1ce90c58a
Andriy Mykhaylyk <erp.lsf@gmail.com>
2024-11-22 11:54:55 +0100

    add inital zone parsing

--- src/zone.zig
+++ src/zone.zig
@@ -18,7 +18,7 @@ pub fn get_next_entry(allocator: std.mem.Allocator, reader: anytype) !?[]u8 {
     if (maybe_line) |line| {
         defer allocator.free(line);
 
-        if (line.len == 0) return line;
+        if (line.len == 0) return line; // it's safe to return (and free) a zero-sized slice
 
         var buffer = std.ArrayListUnmanaged(u8){};
         defer buffer.deinit(allocator);
@@ -70,69 +70,72 @@ pub fn parse_zone_file(allocator: std.mem.Allocator, path: []const u8) !Zone {
     const file = try std.fs.cwd().openFile(path, .{});
     const reader = file.reader();
 
+    var count: usize = 0;
     while (try get_next_entry(allocator, reader)) |entry| : (allocator.free(entry)) {
         if (entry.len == 0) continue;
 
-        std.debug.print("{s}\n", .{entry});
+        count += 1;
+
+        var it = std.mem.splitScalar(u8, entry, ' ');
+
+        var name: []const u8 = undefined;
+        if (it.next()) |n| {
+            name = n;
+        }
+
+        var ttl_found: bool = false;
+        var class_found: bool = false;
+
+        var ttl: usize = undefined;
+        var class: M.Class = undefined;
+
+        if (it.next()) |ttl_or_class| {
+            const maybe_ttl = std.fmt.parseUnsigned(usize, ttl_or_class, 10);
+            if (maybe_ttl) |t| {
+                ttl = t;
+                ttl_found = true;
+            } else |_| {
+                const maybe_class = std.meta.stringToEnum(M.Class, ttl_or_class);
+                if (maybe_class) |c| {
+                    class = c;
+                    class_found = true;
+                } else {
+                    return ZoneParsingError.SecondFieldIsNotATTLNorAClass;
+                }
+            }
+        }
+
+        if (it.next()) |ttl_or_class| {
+            if (ttl_found) {
+                const maybe_class = std.meta.stringToEnum(M.Class, ttl_or_class);
+                if (maybe_class) |c| {
+                    class = c;
+                } else {
+                    return ZoneParsingError.ThirdFieldIsNotAClass;
+                }
+            } else { // it's definitely a ttl
+                const maybe_ttl = std.fmt.parseUnsigned(usize, ttl_or_class, 10);
+                if (maybe_ttl) |t| {
+                    ttl = t;
+                } else |_| {
+                    return ZoneParsingError.ThirdFieldIsNotATTL;
+                }
+            }
+        }
+
+        var @"type": T.Type = undefined;
+        if (it.next()) |t| {
+            const maybe_type = std.meta.stringToEnum(T.Type, t);
+            if (maybe_type) |tt| {
+                @"type" = tt;
+            } else {
+                return ZoneParsingError.FourthFieldIsNotAType;
+            }
+        }
+
+        std.debug.print("entry: {s}\n", .{entry});
+        std.debug.print("name: {s}, class: {}, type: {}, ttl: {d}\n", .{ name, class, @"type", ttl });
     }
-
-    // var it = std.mem.splitScalar(u8, buffer.items, ' ');
-
-    // if (it.next()) |name| {
-    //     std.debug.print("name: {s}\n", .{name});
-    // }
-
-    // var ttl_found: bool = false;
-    // var class_found: bool = false;
-
-    // var ttl: usize = undefined;
-    // var class: M.Class = undefined;
-
-    // if (it.next()) |ttl_or_class| {
-    //     const maybe_ttl = std.fmt.parseUnsigned(usize, ttl_or_class, 10);
-    //     if (maybe_ttl) |t| {
-    //         ttl = t;
-    //         ttl_found = true;
-    //     } else |_| {
-    //         const maybe_class = std.meta.stringToEnum(M.Class, ttl_or_class);
-    //         if (maybe_class) |c| {
-    //             class = c;
-    //             class_found = true;
-    //         } else {
-    //             return ZoneParsingError.SecondFieldIsNotATTLNorAClass;
-    //         }
-    //     }
-    // }
-
-    // if (it.next()) |ttl_or_class| {
-    //     if (ttl_found) {
-    //         const maybe_class = std.meta.stringToEnum(M.Class, ttl_or_class);
-    //         if (maybe_class) |c| {
-    //             class = c;
-    //         } else {
-    //             return ZoneParsingError.ThirdFieldIsNotAClass;
-    //         }
-    //     } else { // it's definitely a ttl
-    //         const maybe_ttl = std.fmt.parseUnsigned(usize, ttl_or_class, 10);
-    //         if (maybe_ttl) |t| {
-    //             ttl = t;
-    //         } else |_| {
-    //             return ZoneParsingError.ThirdFieldIsNotATTL;
-    //         }
-    //     }
-    // }
-
-    // var @"type": T.Type = undefined;
-    // if (it.next()) |t| {
-    //     const maybe_type = std.meta.stringToEnum(T.Type, t);
-    //     if (maybe_type) |tt| {
-    //         @"type" = tt;
-    //     } else {
-    //         return ZoneParsingError.FourthFieldIsNotAType;
-    //     }
-    // }
-
-    // std.debug.print("{s}\n", .{buffer.items});
 
     return zone;
 }
